babb71f9a6a544540740faf3252e232016fe6585
Michael Corey <corey101@umn.edu>
2026-03-03 11:39:11 -0600

    Basic index template

--- apps/photo/views.py
+++ apps/photo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 
-# Create your views here.
+from .models import Photo, STATUS_CHOICES
+
+
+@login_required(login_url="/admin/login/")
+def index(request):
+
+    status_lookup = {s[0]: s[1] for s in STATUS_CHOICES}
+
+    status_counts = Photo.objects.all().values("status").annotate(status_count=Count("id")).order_by('-status_count')
+    status_counts = [{
+        'status_code': item['status'],
+        'status_long': status_lookup[item['status']],
+        'status_count': item['status_count']
+    } for item in list(status_counts)]
+    print(status_counts)
+    context = {'status_counts': status_counts}
+
+    return render(request, "index.html", context)
